521dcd4e82d8d4fd21a892084b51fe78e8c78a4a
Jose Garcia <jagarcia7655@gmail.com>
2021-04-21 23:58:03 -0500

    Aprendiendo hashes y optimización

--- js-portfolio/webpack.config.js
+++ js-portfolio/webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const TercerWebpackPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    assetModuleFilename: "assets/images/[hash][ext][query]",
+    filename: "main[contenthash].js",
+    assetModuleFilename: "assets/images/[name][hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
   },
   module: {
     rules: [
@@ -35,9 +43,9 @@ module.exports = {
       {
         test: /\.(woff|woff2)$/,
         type: "asset/resource",
-        generator: {
-          filename: "assets/fonts/[name][ext]",
-        },
+        // generator: {
+        //   filename: "assets/fonts/[name][hash][ext]",
+        // },
       },
     ],
   },
@@ -45,9 +53,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name][contenthash].css",
+    }),
   ],
   optimization: {
     minimize: true,
+    minimizer: [new CssMinizerWebpackPlugin(), new TercerWebpackPlugin()],
   },
 };
